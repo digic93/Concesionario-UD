d3e97aaa95acefc85505327a1a49ba5369941889
Santiago96 <santicollazosbarrera@hotmail.com>
2017-12-03 17:07:42 -0500

    Cotizacion Finalizada
    Se completa los insert de la cotizacion, proceso y parteauto
    falta generar el pdf

--- src/java/com/udistrital/ConcesionarioUD/control/dao/ProcesoDAO.java
+++ src/java/com/udistrital/ConcesionarioUD/control/dao/ProcesoDAO.java
@@ -8,6 +8,7 @@ package com.udistrital.ConcesionarioUD.control.dao;
 import com.udistrital.ConcesionarioUD.conexion.Conexion;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
 
 /**
  *
@@ -33,6 +34,27 @@ public class ProcesoDAO extends AbstractDao {
     @Override
     public Object getEntityByResultSet(ResultSet resultSet) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int guardar(int idEmpelado,int numCotizacion) {
+        int r = 0;
+        Calendar calendar = Calendar.getInstance();
+        String fecha = calendar.get(Calendar.DATE) + "/" + (calendar.get(Calendar.MONTH)+1) + "/" + calendar.get(Calendar.YEAR);
+        
+        String insert = "INSERT INTO PROCESO (IDPROCESO,IDTIPOPROCESO,IDEMPELADO,IDCOTIZACION,FECHAINICIOPROCESO,FECHAFINALIZACIONPROCESO) VALUES (null,1,"+idEmpelado+","+numCotizacion+",to_date('"+fecha+"','DD/MM/YYYY')"+",to_date('"+fecha+"','DD/MM/YYYY'))";
+        System.out.println(insert);
+        try {
+            this.connection = Conexion.getConexion();
+            this.statement = connection.createStatement();
+            r = this.statement.executeUpdate(insert);
+            statement.close();
+        } catch (SQLException ex) {
+            System.out.println("No se pudo realizar el insert: "+ex.getMessage());
+
+        } finally {
+            Conexion.desconectar();
+        }
+        return r;
     }
 
 }
